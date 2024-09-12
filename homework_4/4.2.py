2473fc2ffd24eb58baaada3be167f0154fc1204e
Volodymyre <vova89.89@i.ua>
2024-09-12 18:28:34 +0300

    work 4 corrected

--- homework_4/4.2.py
+++ homework_4/4.2.py
@@ -1,11 +1,13 @@
-lst=[1, 3, 5] #=> 30
-#lst=[6] #=> 36
+#lst=[1, 3, 5] #=> 30
+lst=[6] #=> 36
 #lst=[] #=> 0
 
 new_lst=[]
 i=0
-while i < len(lst):
-    new_lst.append(lst[i])
-    i+=2
-
-print(sum(new_lst)*lst[-1])
+if not lst:
+    print(0)
+else:
+    while i < len(lst):
+        new_lst.append(lst[i])
+        i+=2
+    print(sum(new_lst)*lst[-1])
