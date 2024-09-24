4a5f06b0b2ae9de21257f57e6eb6e224ee79e429
Volodymyre <vova89.89@i.ua>
2024-09-24 09:21:50 +0300

    work 7.3

---
+++ homework_7/7.3.py
@@ -0,0 +1,16 @@
+from operator import index
+
+def second_index(text, some_str):
+    if text.count(some_str) <=1:
+        return None
+    else:
+        ind = text.find(some_str)
+        ind = text.find(some_str, ind+1)
+        return ind
+
+
+assert second_index("sims", "s") == 3, 'Test1'
+assert second_index("find the river", "e") == 12, 'Test2'
+assert second_index("hi", "h") is None, 'Test3'
+assert second_index("Hello, hello", "lo") == 10, 'Test4'
+print('ОК')
